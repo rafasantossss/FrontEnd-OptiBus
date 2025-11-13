4abfebcf2fba75c8f3f3594793db5e3ebc63c7ff
JOÃO GENTIL SCARABELOT <joao_scarabelot@estudante.sesisenai.org.br>
2025-11-13 09:18:07 -0300

    Finalizado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,8 @@ import "react-native-reanimated";
 import { useColorScheme } from "react-native";
 
 export const unstable_settings = {
-  anchor: "(tabs)",
+ 
+  initialRouteName: "index",
 };
 
 export default function RootLayout() {
@@ -17,11 +18,15 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="registro" options={{ title: "Cadastro de Usuário" }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
+        
+        
+        <Stack.Screen 
+          name="modal" 
+          options={{ presentation: "modal", title: "Modal" }} 
+        />
       </Stack>
-      {/* ⚠️ AQUI ESTAVA O ERRO ⚠️ */}
-      {/* O estilo da StatusBar agora se ajusta à cor do tema: se for 'dark', a barra fica 'light' (texto branco), e vice-versa. */}
-      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} /> 
+
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </ThemeProvider>
   );
 }
